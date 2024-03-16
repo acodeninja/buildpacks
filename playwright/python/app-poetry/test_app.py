f96a8c9d5492ae1d9fd8b9f8134ca9f6c780fe10
Lawrence <lawrence@acode.ninja>
2024-03-16 08:49:21 +0000

    fix: use example.com domain instead of playwright for tests

--- playwright/python/app-poetry/test_app.py
+++ playwright/python/app-poetry/test_app.py
@@ -1,17 +1,20 @@
+import os
 import re
 from playwright.sync_api import Page, expect
 
+
 def test_has_title(page: Page):
-    page.goto("https://playwright.dev/")
+    page.goto("https://example.com/")
 
     # Expect a title "to contain" a substring.
-    expect(page).to_have_title(re.compile("Playwright"))
+    expect(page).to_have_title(re.compile("Example Domain"))
+
 
 def test_get_started_link(page: Page):
-    page.goto("https://playwright.dev/")
+    page.goto("https://example.com/")
 
     # Click the get started link.
-    page.get_by_role("link", name="Get started").click()
+    page.get_by_role("link", name="More information...").click()
 
     # Expects page to have a heading with the name of Installation.
-    expect(page.get_by_role("heading", name="Installation")).to_be_visible()
+    expect(page.get_by_role("heading", name="Example Domains")).to_be_visible()
