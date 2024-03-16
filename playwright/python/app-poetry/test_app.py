ad6a8a55d600ede377e69a01c42fd24996026e0b
Lawrence <lawrence@acode.ninja>
2024-03-16 08:56:55 +0000

    fix: only hit example.com main page

--- playwright/python/app-poetry/test_app.py
+++ playwright/python/app-poetry/test_app.py
@@ -8,13 +8,3 @@ def test_has_title(page: Page):
 
     # Expect a title "to contain" a substring.
     expect(page).to_have_title(re.compile("Example Domain"))
-
-
-def test_get_started_link(page: Page):
-    page.goto("https://example.com/")
-
-    # Click the get started link.
-    page.get_by_role("link", name="More information...").click()
-
-    # Expects page to have a heading with the name of Installation.
-    expect(page.get_by_role("heading", name="Example Domains")).to_be_visible()
