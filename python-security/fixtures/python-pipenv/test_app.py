a1257ecad4fac0424af95d19b0c9f38ddee019e2
Lawrence <lawrence@acode.ninja>
2024-06-19 08:32:35 +0100

    fix: add xmlsec1 binary and libxmlsec

--- python-security/fixtures/python-pipenv/test_app.py
+++ python-security/fixtures/python-pipenv/test_app.py
@@ -1,11 +1,17 @@
 import xmlsec
+from shutil import which
+
 
 def test_sign():
     data = b'\xa8f4dP\x82\x02\xd3\xf5.\x02\xc1\x03\xef\xc4\x86\xabC\xec\xb7>\x8e\x1f\xa3\xa3\xc5\xb9qc\xc2\x81\xb1-\xa4B\xdf\x03>\xba\xd1'
 
     ctx = xmlsec.SignatureContext()
-    key = xmlsec.Key.from_file('keys/id_rsa.pem', xmlsec.constants.KeyDataFormatPem)
+    key = xmlsec.Key.from_file('/workspace/keys/id_rsa.pem', xmlsec.constants.KeyDataFormatPem)
     ctx.key = key
     sign = ctx.sign_binary(data, xmlsec.constants.TransformRsaSha1)
 
     print(sign)
+
+
+def test_xmlsec1_binary_is_installed():
+    assert which("xmlsec1") == '/layers/acodeninja_python-security/apt/usr/bin/xmlsec1'
