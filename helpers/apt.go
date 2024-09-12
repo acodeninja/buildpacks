7cca696e30ee2644ad67bfb81da0a88d4e458902
Lawrence <lawrence@acode.ninja>
2024-09-12 18:55:02 +0100

    feat: allow setting apt installer to inject build or shared

--- helpers/apt.go
+++ helpers/apt.go
@@ -13,7 +13,7 @@ import (
 	"strings"
 )
 
-func InstallAptPackages(layer libcnb.Layer, packageList []string, logger bard.Logger) error {
+func InstallAptPackages(layer libcnb.Layer, packageList []string, logger bard.Logger, buildOnly bool) error {
 	var err error
 
 	logger.Headerf("Installing APT packages in %s layer", layer.Name)
@@ -84,16 +84,29 @@ func InstallAptPackages(layer libcnb.Layer, packageList []string, logger bard.Lo
 	logger.Header("  Installing APT packages")
 	err = dpkgInstall(IndentedWriterFactory(2, logger), aptCacheDirectory, aptFolder)
 
-	layer.SharedEnvironment.Prependf("PATH", ":", "%s/usr/bin:%s/bin", aptFolder, aptFolder)
+	if buildOnly {
+		layer.BuildEnvironment.Prependf("PATH", ":", "%s/usr/bin:%s/bin", aptFolder, aptFolder)
 
-	libPath := fmt.Sprintf("%s/lib/x86_64-linux-gnu:%s/lib/i386-linux-gnu:%s/lib:%s/usr/lib/x86_64-linux-gnu:%s/usr/lib/i386-linux-gnu:%s/usr/lib", aptFolder, aptFolder, aptFolder, aptFolder, aptFolder, aptFolder)
-	layer.SharedEnvironment.Prepend("LD_LIBRARY_PATH", ":", libPath)
-	layer.SharedEnvironment.Prepend("LIBRARY_PATH", ":", libPath)
+		libPath := fmt.Sprintf("%s/lib/x86_64-linux-gnu:%s/lib/i386-linux-gnu:%s/lib:%s/usr/lib/x86_64-linux-gnu:%s/usr/lib/i386-linux-gnu:%s/usr/lib", aptFolder, aptFolder, aptFolder, aptFolder, aptFolder, aptFolder)
+		layer.BuildEnvironment.Prepend("LD_LIBRARY_PATH", ":", libPath)
+		layer.BuildEnvironment.Prepend("LIBRARY_PATH", ":", libPath)
 
-	includePath := fmt.Sprintf("%s/usr/include:%s/usr/include/x86_64-linux-gnu", aptFolder, aptFolder)
-	layer.SharedEnvironment.Prepend("INCLUDE_PATH", ":", includePath)
-	layer.SharedEnvironment.Prepend("CPATH", ":", includePath)
-	layer.SharedEnvironment.Prepend("CPPPATH", ":", includePath)
+		includePath := fmt.Sprintf("%s/usr/include:%s/usr/include/x86_64-linux-gnu", aptFolder, aptFolder)
+		layer.BuildEnvironment.Prepend("INCLUDE_PATH", ":", includePath)
+		layer.BuildEnvironment.Prepend("CPATH", ":", includePath)
+		layer.BuildEnvironment.Prepend("CPPPATH", ":", includePath)
+	} else {
+		layer.SharedEnvironment.Prependf("PATH", ":", "%s/usr/bin:%s/bin", aptFolder, aptFolder)
+
+		libPath := fmt.Sprintf("%s/lib/x86_64-linux-gnu:%s/lib/i386-linux-gnu:%s/lib:%s/usr/lib/x86_64-linux-gnu:%s/usr/lib/i386-linux-gnu:%s/usr/lib", aptFolder, aptFolder, aptFolder, aptFolder, aptFolder, aptFolder)
+		layer.SharedEnvironment.Prepend("LD_LIBRARY_PATH", ":", libPath)
+		layer.SharedEnvironment.Prepend("LIBRARY_PATH", ":", libPath)
+
+		includePath := fmt.Sprintf("%s/usr/include:%s/usr/include/x86_64-linux-gnu", aptFolder, aptFolder)
+		layer.SharedEnvironment.Prepend("INCLUDE_PATH", ":", includePath)
+		layer.SharedEnvironment.Prepend("CPATH", ":", includePath)
+		layer.SharedEnvironment.Prepend("CPPPATH", ":", includePath)
+	}
 
 	return nil
 }
@@ -195,7 +208,7 @@ func (apt AptLayer) Contribute(layer libcnb.Layer) (libcnb.Layer, error) {
 		}
 		layer.Metadata["packages"] = apt.Packages
 
-		err := InstallAptPackages(layer, apt.Packages, apt.Logger)
+		err := InstallAptPackages(layer, apt.Packages, apt.Logger, false)
 
 		return layer, err
 	})
