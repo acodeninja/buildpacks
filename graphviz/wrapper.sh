4d800def9f0d6893ec34a4dbceef8276f1ec08c8
Lawrence <34475808+acodeninja@users.noreply.github.com>
2024-11-19 11:21:11 +0000

    fix: argument expansion in bash wrapper (#24)

--- graphviz/wrapper.sh
+++ graphviz/wrapper.sh
@@ -5,4 +5,4 @@ LD_LIBRARY_PATH="{{range $val := .LibLocations}}{{$val}}:{{end}}$LD_LIBRARY_PATH
 LIBRARY_PATH="{{range $val := .LibLocations}}{{$val}}:{{end}}$LD_LIBRARY_PATH"  \
 FONTCONFIG_PATH="{{.FontConfigLocation}}" \
 GVBINDIR="{{.GraphvizBinDir}}" \
-{{.Command}} $@
+{{.Command}} "$@"
